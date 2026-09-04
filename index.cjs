a068f41beef3549df7b87790f8ef369332c19849
KishoreKumar0407 <kishorekumar0407@github.com>
2026-09-04 22:50:55 +0530

    Fix: add explicit Access-Control-Allow-Origin headers and 200 OK preflight handler for OPTIONS requests

--- index.cjs
+++ index.cjs
@@ -21,6 +21,17 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
